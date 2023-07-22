9c37c35be693351969672c1f94c1082fe9422b24
danilkk <ezzfvkoko@gmail.com>
2023-07-22 20:52:22 +0300

    pls work

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import AppRouter from "./AppRouter";
 
 function App() {
     return (
-        <BrowserRouter basename={"/3d-react-calculator/"}>
+        <BrowserRouter basename={"3d-react-calculator/"}>
             <div className="app">
                 <AppRouter/>
             </div>
