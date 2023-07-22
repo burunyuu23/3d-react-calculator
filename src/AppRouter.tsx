1c485bb234a11bba76c3906ef2398fba96a284f2
danilkk <ezzfvkoko@gmail.com>
2023-07-22 21:14:14 +0300

    pls work

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -6,7 +6,7 @@ import ThreeScene from "./views/ThreeScene";
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="3d-react-calculator/"
+            <Route path="/"
                    element={<CalcScene />} />
             <Route path="/test"
                    element={<ThreeScene />} />
